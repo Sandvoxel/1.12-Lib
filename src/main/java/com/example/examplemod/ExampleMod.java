27da68e0dd489c27e5d35bf7a2a3b7668c49f6ed
Sandvoxel <kovalchickzz@gmail.com>
2017-09-30 01:21:37 -0500

    bacic setup

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -4,17 +4,30 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+@Mod(modid = Refrence.MODID, version = Refrence.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+
+
+
+
+
     
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+
+    }
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+    @EventHandler
+    public void preInit(FMLPostInitializationEvent event){
+
     }
 }
---
+++ src/main/java/com/example/examplemod/Refrence.java
@@ -0,0 +1,7 @@
+package com.example.examplemod;
+
+public class Refrence {
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+
+}
